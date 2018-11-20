ecf968b6789c40875c9dd14e75ca53e2bef6149d
Partanennn <aleksi.partanenn17@gmail.com>
2018-11-20 14:44:10 +0200

    Added inserts.sql

--- SQL/Database.sql
+++ SQL/Database.sql
@@ -27,7 +27,3 @@ CREATE TABLE kanta.machines
     category varChar(20) NOT NULL
 )
 ENGINE = InnoDB;
-
-INSERT INTO users(name, streetAddress, city, email, username, pass, role) VALUES("Testi Jäbä", "Microkatu 1", "Kuopio", "testi@email.fi", "testi", "testi", "admin");
-INSERT INTO users(name, streetAddress, city, email, username, pass) VALUES("Erkki Pesonen", "Microkatu 5", "Kuopio", "pesoserkki@uef.fi", "erkki", "erkki");
-INSERT INTO users(name, streetAddress, city, email, username, pass) VALUES("Paavo Pakoma", "Microkatu 66", "Kuopio", "puavo@ueffi.fi", "paavo", "pakoma");
---
+++ SQL/inserts.sql
@@ -0,0 +1,3 @@
+INSERT INTO users(name, streetAddress, city, email, username, pass, role) VALUES("Testi Jäbä", "Microkatu 1", "Kuopio", "testi@email.fi", "testi", "testi", "admin");
+INSERT INTO users(name, streetAddress, city, email, username, pass) VALUES("Erkki Pesonen", "Microkatu 5", "Kuopio", "pesoserkki@uef.fi", "erkki", "erkki");
+INSERT INTO users(name, streetAddress, city, email, username, pass) VALUES("Paavo Pakoma", "Microkatu 66", "Kuopio", "puavo@ueffi.fi", "paavo", "pakoma");
