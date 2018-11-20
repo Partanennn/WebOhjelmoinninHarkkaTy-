ae91b4df50c31107f1a2b3dcecf797dfe4ff61cb
joonav <44670910+joonav@users.noreply.github.com>
2018-11-20 17:35:51 +0200

    Tietokantaan lisätty tuotteita 7kpl

--- SQL/inserts.sql
+++ SQL/inserts.sql
@@ -1,3 +1,12 @@
 INSERT INTO users(name, streetAddress, city, email, username, pass, role) VALUES("Testi Jäbä", "Microkatu 1", "Kuopio", "testi@email.fi", "testi", "testi", "admin");
 INSERT INTO users(name, streetAddress, city, email, username, pass) VALUES("Erkki Pesonen", "Microkatu 5", "Kuopio", "pesoserkki@uef.fi", "erkki", "erkki");
 INSERT INTO users(name, streetAddress, city, email, username, pass) VALUES("Paavo Pakoma", "Microkatu 66", "Kuopio", "puavo@ueffi.fi", "paavo", "pakoma");
+
+INSERT INTO machines(name, model, brand, description_text, location, owner, category) VALUES("HP Pavilion 570-p038no", "Pavilion 570-p038no", "HP", "Kelpo tietokone", "B2002", "Savonia", "Tietokone");
+INSERT INTO machines(name, model, brand, description_text, location, owner, category) VALUES("HP Pavilion 570-p038no", "Pavilion 570-p038no", "HP", "Kelpo tietokone", "B2002", "Savonia", "Tietokone");
+INSERT INTO machines(name, model, brand, description_text, location, owner, category) VALUES("HP Pavilion 570-p038no", "Pavilion 570-p038no", "HP", "Kelpo tietokone", "e26-27", "UEF", "Tietokone");
+INSERT INTO machines(name, model, brand, description_text, location, owner, category) VALUES("Brother DCP-L2537DW AIO" "DCP-L2537DW AIO", "Brother", "Savonian tulostin mikrokadulla", "B2002", "Savonia", "Tulostin");
+INSERT INTO machines(name, model, brand, description_text, location, owner, category) VALUES("Brother DCP-L2537DW AIO" "DCP-L2537DW AIO", "Brother", "Yliopiston tulostin luokassa e26-27", "e26-27", "UEF", "Tulostin");
+INSERT INTO machines(name, model, brand, description_text, location, owner, category) VALUES("MacBook Air MQD32", "MQD32", "MacBook Air", "Tää on aito mäkbuuk", "SN-kirjasto", "UEF", "Kannettava");
+INSERT INTO machines(name, model, brand, description_text, location, owner, category) VALUES("MacBook Air MQD32", "MQD32", "MacBook Air", "Tää on aito mäkbuuk", "SN-kirjasto", "UEF", "Kannettava");
+INSERT INTO machines(name, model, brand, description_text, location, owner, category) VALUES("Huawei MediaPad T3", "MediaPad T3", "Huawei", "Tabletti Snellmanian kirjastossa", "SN-kirjasto", "UEF", "Tabletti");
